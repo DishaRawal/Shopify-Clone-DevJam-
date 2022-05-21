476a99d6034d9e80eff809f3ff0edaba84150d5f
Disha <disharawal03@gmail.com>
2022-05-21 18:47:07 +0530

    basic code

---
+++ app.js
@@ -0,0 +1,45 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res) {
+
+    res.render("home");
+
+})
+
+app.get("/seller", function(req, res) {
+
+    res.render("seller");
+
+})
+
+app.get("/contact", function(req, res) {
+
+    res.render("contact");
+
+})
+
+app.get("/byer", function(req, res) {
+
+    res.render("byer");
+
+})
+
+app.post("/", function(req, res) {
+
+
+})
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
